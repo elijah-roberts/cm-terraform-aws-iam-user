cd4955608715fb9e378635dbcc5dbe772c58ae1b
Elijah Roberts <eroberts@verifiedfirst.com>
2019-12-12 13:48:42 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
 resource "aws_iam_user" "default" {
-  count = "${var.enabled == "true" ? 1 : 0}"
+  count = var.enabled == "true" ? 1 : 0
 
-  name                 = "${var.name}"
-  path                 = "${var.path}"
-  permissions_boundary = "${var.permissions_boundary}"
-  force_destroy        = "${var.force_destroy}"
+  name                 = var.name
+  path                 = var.path
+  permissions_boundary = var.permissions_boundary
+  force_destroy        = var.force_destroy
 }
 
 resource "aws_iam_user_login_profile" "default" {
-  count = "${var.enabled == "true" && var.login_profile_enabled == "true" ? 1 : 0}"
+  count = var.enabled == "true" && var.login_profile_enabled == "true" ? 1 : 0
 
-  user                    = "${aws_iam_user.default[count.index].name}"
-  pgp_key                 = "${var.pgp_key}"
-  password_length         = "${var.password_length}"
-  password_reset_required = "${var.password_reset_required}"
+  user                    = aws_iam_user.default[count.index].name
+  pgp_key                 = var.pgp_key
+  password_length         = var.password_length
+  password_reset_required = var.password_reset_required
   depends_on              = ["aws_iam_user.default"]
 }
 
 resource "aws_iam_access_key" "default" {
-  count                   = "${var.enabled == "true" && var.access_keys_enabled == "true" ? 1 : 0}"
-  user                    = "${aws_iam_user.default[count.index].name}"
-  pgp_key                 = "${var.pgp_key}"
-  status                  = "${var.access_key_status}"
+  count                   = var.enabled == "true" && var.access_keys_enabled == "true" ? 1 : 0
+  user                    = aws_iam_user.default[count.index].name
+  pgp_key                 = var.pgp_key
+  status                  = var.access_key_status
 }
 
 resource "aws_iam_user_group_membership" "default" {
-  count      = "${var.enabled == "true" && length(var.groups) > 0 ? 1 : 0}"
-  user       = "${aws_iam_user.default[count.index].name}"
+  count      = var.enabled == "true" && length(var.groups) > 0 ? 1 : 0
+  user       = aws_iam_user.default[count.index].name
   groups     = var.groups
   depends_on = ["aws_iam_user.default"]
 }
@@ -47,7 +47,7 @@ data "template_file" "keybase_password_decrypt_command" {
   template = "${file("${path.module}/templates/decrypt_command.sh")}"
 
   vars = {
-    encrypted_data = "${local.encrypted_password}"
+    encrypted_data = local.encrypted_password
   }
 }
 
@@ -55,7 +55,7 @@ data "template_file" "keybase_secret_access_key_decrypt_command" {
   template = "${file("${path.module}/templates/decrypt_command.sh")}"
 
   vars = {
-    encrypted_data = "${local.encrypted_secret_access_key}"
+    encrypted_data = local.encrypted_secret_access_key
   }
 }
 
@@ -63,7 +63,7 @@ data "template_file" "keybase_password_pgp_message" {
   template = "${file("${path.module}/templates/pgp_message.txt")}"
 
   vars = {
-    encrypted_data = "${local.encrypted_password}"
+    encrypted_data = local.encrypted_password
   }
 }
 
@@ -71,7 +71,7 @@ data "template_file" "keybase_secret_access_key_pgp_message" {
   template = "${file("${path.module}/templates/pgp_message.txt")}"
 
   vars = {
-    encrypted_data = "${local.encrypted_secret_access_key}"
+    encrypted_data = local.encrypted_secret_access_key
   }
 
 }
